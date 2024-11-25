4534a539abce8d2ba7985816f14a9aef53c6eb00
yonatanvolog <yonatan.vologdin@gmail.com>
2024-11-25 09:21:47 +0200

    Finished chapter 3 a

---
+++ Assets/Scripts/LifeTotalScript.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class LifeTotalScript : MonoBehaviour
+{
+    [SerializeField] private float initialLifeTotal = 100f;
+    private float lifeTotal;
+
+    private void Start()
+    {
+        lifeTotal = initialLifeTotal;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        lifeTotal -= damage;
+        if (lifeTotal <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Heal(float healAmount)
+    {
+        lifeTotal = Mathf.Min(lifeTotal + healAmount, initialLifeTotal);
+    }
+
+    public float GetLifeTotal()
+    {
+        return lifeTotal;
+    }
+}
