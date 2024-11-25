5e2224563f21a3bae3207dc3d5209e66689793b6
yonatanvolog <yonatan.vologdin@gmail.com>
2024-11-25 17:27:14 +0200

    code refactor

--- Assets/Scripts/ShootyGunScript3D.cs
+++ Assets/Scripts/ShootyGunScript3D.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 
 public class ShootyGunScript3D : MonoBehaviour
 {
-    [SerializeField] private GameObject bulletPrefab; // Prefab for the bullet
-    [SerializeField] private float bulletSpeed = 20f; // Speed of the bullet
-    [SerializeField] private float bulletLifetime = 5f; // Time before the bullet is destroyed
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private float bulletSpeed = 20f;
+    [SerializeField] private float bulletLifetime = 5f;
 
     private void Update()
     {
@@ -17,15 +17,10 @@ public class ShootyGunScript3D : MonoBehaviour
 
     private void Shoot()
     {
-        // Instantiate the bullet at the camera's position and orientation
         GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
         bullet.SetActive(true);
-
-        // Add velocity in the forward direction of the camera
         Rigidbody bulletRb = bullet.GetComponent<Rigidbody>();
         bulletRb.velocity = transform.forward * bulletSpeed;
-
-        // Destroy the bullet after a certain time
         Destroy(bullet, bulletLifetime);
     }
 }
